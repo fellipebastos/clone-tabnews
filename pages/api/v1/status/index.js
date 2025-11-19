438959b9c62dbe982aa9d4e9f04c9d8ecbeba432
Fellipe Bastos <fellipebastos@192.168.0.4>
2025-11-19 17:14:48 -0300

    add `database.js` and `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import "../../../../infra/database.js";
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  await database.query("SELECT 1 + 1 as sum;");
   response.status(200).json({ status: "ok" });
 }
 
